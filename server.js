aad8e6d7636ef34251d4dfe87cade0aac855ca46
vbird <venetiabird@gmail.com>
2025-07-18 16:04:02 +1000

    chore add template

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http')
 const fs = require('fs');
 const path = require('path');
 const { URLSearchParams } = require('url');
+const ejs = require('ejs');
 
 const port = 3000;
 
@@ -13,11 +14,16 @@ const server = http.createServer((req, res) => {
         res.end(`<div id="response-wrapper" class="${colourClass}"><div id="response"</div>
             </div>`)
     } else if(req.method === "GET" && req.url === "/data") {
-        const filePath = path.join(__dirname, 'data.html');
-        fs.readFile(filePath, 'utf8', (err, data) => {
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.end(data)
-        })
+        const filePath = path.join(__dirname, 'data.ejs');
+        ejs.renderFile(filePath, { data: { name: "Jon smith"} }, {}, function(err, str){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(str)
+        });
+
+        // fs.readFile(filePath, 'utf8', (err, data) => {
+        //         res.writeHead(200, {"Content-Type": "text/html"});
+        //         res.end(data)
+        // })
     
     } else if(req.method === "GET" && req.url === "/") {
         const filePath = path.join(__dirname, 'index.html');
@@ -30,6 +36,8 @@ const server = http.createServer((req, res) => {
         req.on("data", chunk => {
             body += chunk
         })
+
+        console.log("body", body);
         
         req.on("end", () => {
             const params = new URLSearchParams(body);
