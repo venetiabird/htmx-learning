2a71e30049ad267edc4bfa4b89bad946080f139c
vbird <2009687+venetiabird@users.noreply.github.com>
2025-10-17 16:06:30 +1100

    add plant selection page

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import Fastify from 'fastify'
+import formBody from '@fastify/formbody'
+import fs from 'fs'
+import path from 'path'
+
+// https://www.gardeningaustraliamag.com.au/planting-guide/
 
 const fastify = Fastify({
     logger: true
 })
 
+fastify.register(formBody)
+
 fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
+    // reply.send({ hello: 'world' })
+  let html = fs.readFileSync(path.resolve(path.dirname(''), 'src/pages/climate-selection.html'), 'utf8')
+  reply.header('Content-Type', 'text/html; charset=utf-8').send(html)
+})
+
+fastify.post('/plant-selection', function(request, reply) {
+  let html = fs.readFileSync(path.resolve(path.dirname(''), 'src/pages/plant-selection.html'), 'utf8')
+  reply.header('Content-Type', 'text/html; charset=utf-8').send(html)
 })
 
 fastify.listen({ port: 3000 }, function (err, address) {
