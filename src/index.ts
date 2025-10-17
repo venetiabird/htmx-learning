2322fb8921d3a8e452315338b4e62a49aa88e237
Patrick Stockwell <pat.stockwell@rea-group.com>
2025-10-17 15:22:13 +1100

    Add fastify

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-console.log("Hi there from TypeScript");
+import Fastify from 'fastify'
 
 // https://www.gardeningaustraliamag.com.au/planting-guide/
-// TODO: Add fastify!
+// TODO: Render the climate-selection html page
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async (_request, _reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
